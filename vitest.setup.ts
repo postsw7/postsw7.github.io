8221721462e7861957d56ee8fde81eac39399097
Siwoo Lee <slee1009@wgu.edu>
2025-10-08 20:48:42 -0400

    chore(husky): remove deprecated context

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,16 @@
+/* eslint-disable */
 import '@testing-library/jest-dom'
 import { vi } from 'vitest'
 
 // Mock analytics to avoid side effects
 vi.mock('./src/core/analytics', () => ({
-  trackVisit: () => { console.log('mock trackVisit') },
-  trackCommand: () => { console.log('mock trackCommand') },
-  trackUnknownCommand: () => { console.log('mock trackUnknownCommand') },
+  trackVisit: () => {
+    console.log('mock trackVisit')
+  },
+  trackCommand: () => {
+    console.log('mock trackCommand')
+  },
+  trackUnknownCommand: () => {
+    console.log('mock trackUnknownCommand')
+  },
 }))
